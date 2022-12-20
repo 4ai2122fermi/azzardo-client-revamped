c23837df82d316f825b5a389820e0d3c1fa49ddc
heizelnut <upbeatshaw@protonmail.com>
2022-12-20 11:46:46 +0100

    Add Business logic

---
+++ app/src/main/java/com/example/azzardoclient/BetActivity.java
@@ -0,0 +1,129 @@
+package com.example.azzardoclient;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+@SuppressLint("SetTextI18n")
+public class BetActivity extends AppCompatActivity {
+    Thread Thread1 = null;
+    EditText etIP, etPort;
+    TextView tvMessages;
+    EditText etNumber, etStake;
+    Button btnSend;
+    String SERVER_IP;
+    int SERVER_PORT;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etIP = findViewById(R.id.etIP);
+        etPort = findViewById(R.id.etPort);
+        tvMessages = findViewById(R.id.tvMessages);
+        etNumber = findViewById(R.id.number);
+        etStake = findViewById(R.id.stake);
+        btnSend = findViewById(R.id.btnSend);
+        Button btnConnect = findViewById(R.id.btnConnect);
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvMessages.setText("");
+                SERVER_IP = etIP.getText().toString().trim();
+                SERVER_PORT = Integer.parseInt(etPort.getText().toString().trim());
+                Thread1 = new Thread(new Thread1());
+                Thread1.start();
+            }
+        });
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = "PUNTATA," + Integer.parseInt(etStake.getText().toString()) +
+                        "," + Integer.parseInt(etNumber.getText().toString());
+                if (!message.isEmpty()) {
+                    new Thread(new Thread3(message)).start();
+                }
+            }
+        });
+    }
+    private PrintWriter output;
+    private BufferedReader input;
+    class Thread1 implements Runnable {
+        @Override
+        public void run() {
+            Socket socket;
+            try {
+                socket = new Socket(SERVER_IP, SERVER_PORT);
+                output = new PrintWriter(socket.getOutputStream());
+                input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tvMessages.setText("Connected\n");
+                    }
+                });
+                new Thread(new Thread2()).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    class Thread2 implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    final String message = input.readLine();
+                    if (message != null) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tvMessages.append("> " + message + "\r\n");
+                                if (message.startsWith("NOREADY"))
+                                    Toast.makeText(getApplicationContext(), "Partita non ancora iniziata", Toast.LENGTH_LONG).show();
+                            }
+                        });
+                        Toast.makeText(getApplicationContext(), "Prova", Toast.LENGTH_LONG).show();
+
+                    } else {
+                        Thread1 = new Thread(new Thread1());
+                        Thread1.start();
+                        return;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    class Thread3 implements Runnable {
+        private String message;
+        Thread3(String message) {
+            this.message = message;
+        }
+        @Override
+        public void run() {
+            output.write(message + "\r\n");
+            output.flush();
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    tvMessages.append("< " + message + "\r\n");
+                    etStake.setText("");
+                    etNumber.setText("");
+                }
+            });
+        }
+    }
+}
